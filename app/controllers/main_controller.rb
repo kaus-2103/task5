4f16ed691a268d5f094f8cc842bc0e25463bc944
Kaushik Roy <63772738+kaus-2103@users.noreply.github.com>
2024-04-27 17:44:28 +0600

    Fixed all (I hope)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,9 +5,16 @@ class MainController < ApplicationController
   end
 
   def generate_fake_data
-    session[:i] ||= 0
     seed = params[:seed].to_i || Random.new_seed
     region = params[:region] || 'en' 
+    error_value = params[:error_value].to_f  
+
+    session[:i] = 0 if params[:scroll] == 'false'
+
+    puts ">>>>> params <<<<< #{params[:scroll]}"
+    puts ">>>>> session <<<<< #{session[:i]}"
+    puts ">>>>> error <<<<< #{error_value}"
+
 
     case region
     when 'Poland'
@@ -15,33 +22,49 @@ class MainController < ApplicationController
     when 'USA'
       Faker::Config.locale = 'en-US'
     when 'UK'
-      Faker::Config.locale = 'en-UK'
+      Faker::Config.locale = 'en-GB'
     else
       Faker::Config.locale = 'en'
     end
 
-    users = generate_fake_data_for_region(20, seed)
-
-    session[:i] += 0
+    num_user = params[:scroll] == 'true' ? 10 : 20
+    puts ">>>>> number <<<<< #{num_user}"
+    users = generate_fake_data_for_region(num_user, seed, error_value)
 
     render json: users
   end
 
   private
 
-  def generate_fake_data_for_region(num_users, seed)
+  def generate_fake_data_for_region(num_users, seed, error_value)
     faker_rand = Random.new(seed)
     Faker::UniqueGenerator.clear 
     Faker::Config.random = faker_rand 
+    rand = 
     users = []
-    (session[:i]...session[:i] + num_users).each do |index|
-      users << {
-        id: index + 1,
-        name: Faker::Name.name,
-        address: Faker::Address.full_address,
-        phone: Faker::PhoneNumber.phone_number,
-        identifier: Faker::Alphanumeric.alphanumeric(number: 10) 
-      }
+
+    num_users.times do
+      r = rand(0..10)
+      if r <= error_value
+       
+      
+        users << {
+          id: session[:i] + 1,
+          name: Faker::Lorem.word, 
+          address: Faker::Lorem.sentence, 
+          phone: Faker::PhoneNumber.cell_phone,  
+          identifier: Faker::Alphanumeric.alphanumeric(number: 10)
+        }
+      else
+        users << {
+          id: session[:i] + 1,
+          name: Faker::Name.name,
+          address: Faker::Address.full_address,
+          phone: Faker::PhoneNumber.phone_number,
+          identifier: Faker::Alphanumeric.alphanumeric(number: 10)
+        }
+      end
+      session[:i] += 1  
     end
     users
   end
